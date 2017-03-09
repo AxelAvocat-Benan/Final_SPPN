74937131f30f8683f8a93ecad7acdada0a8db3d2
killian AVOCAT-BENAN <killian.avocat@gmail.com>
2017-03-09 21:15:42 +0100

    Scaffold + Carrierwave

---
+++ app/models/membership.rb
@@ -0,0 +1,4 @@
+class Membership < ApplicationRecord
+  belongs_to :user
+  mount_uploader :photo, PhotoUploader
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309200236) do
+ActiveRecord::Schema.define(version: 20170309200615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memberships", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "diplome"
+    t.string   "age"
+    t.string   "activity"
+    t.string   "massif"
+    t.string   "periode"
+    t.boolean  "passmorgiou"
+    t.boolean  "passsormiou"
+    t.string   "siret"
+    t.text     "description"
+    t.integer  "user_id"
+    t.string   "photo"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_memberships_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +51,5 @@ ActiveRecord::Schema.define(version: 20170309200236) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "memberships", "users"
 end
