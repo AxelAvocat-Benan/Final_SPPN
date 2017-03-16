13664a210ccad92185c2dfac253cea13dfd7cd9d
Axel AVOCAT-BENAN <axel.avocat@gmail.com>
2017-03-16 15:08:59 +0100

    devise fr

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module FinalSppn
       generate.assets false
     end
 
+    config.i18n.available_locales = [:fr, :en]
+    config.i18n.default_locale = :fr
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
