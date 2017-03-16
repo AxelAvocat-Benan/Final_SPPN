fedda5bd57e5b59d7211284be04f6ffb8c7f3050
Killian AVOCAT BENAN <killian.avocat@gmail.com>
2017-03-16 16:34:04 +0100

    design card + traduction devise

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,12 @@ class Membership < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :diplome, presence: true
+  validates :age, presence: true
+  validates :activity, presence: true
+  validates :massif, presence: true
+  validates :periode, presence: true
+  validates :siret, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
 end
